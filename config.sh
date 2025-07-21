82a0b5c1e56e47f11ca73179ebeebe7ed1fc8eb8
Alexander <glowar@glowar.ru>
2025-07-22 00:43:30 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-echo "Config"
+echo "Config mqtt"
+sed -i "s/\"server\":\s*\".*\"/\"server\": \"$MQTT_SERVER\"/g" /etc/inverter/mqtt.json
+sed -i "s/\"port\":\s*\".*\"/\"port\": \"$MQTT_PORT\"/g" /etc/inverter/mqtt.json
+sed -i "s/\"topic\":\s*\".*\"/\"topic\": \"$MQTT_TOPIC\"/g" /etc/inverter/mqtt.json
+sed -i "s/\"devicename\":\s*\".*\"/\"devicename\": \"$MQTT_DEVICENAME\"/g" /etc/inverter/mqtt.json
+sed -i "s/\"manufacturer\":\s*\".*\"/\"manufacturer\": \"$MQTT_MANUFACTURER\"/g" /etc/inverter/mqtt.json
+sed -i "s/\"model\":\s*\".*\"/\"model\": \"$MQTT_MODEL\"/g" /etc/inverter/mqtt.json
+sed -i "s/\"serial\":\s*\".*\"/\"serial\": \"$MQTT_SERIAL\"/g" /etc/inverter/mqtt.json
+sed -i "s/\"ver\":\s*\".*\"/\"ver\": \"$MQTT_VER\"/g" /etc/inverter/mqtt.json
+sed -i "s/\"username\":\s*\".*\"/\"username\": \"$MQTT_USERNAME\"/g" /etc/inverter/mqtt.json
 sed -i "s/\"password\":\s*\".*\"/\"password\": \"$MQTT_PASSWORD\"/g" /etc/inverter/mqtt.json
+
+echo "Config inverter"
+sed -i "s/device = .*/device=$INVERTER_DEVICE/g" /etc/inverter/inverter.conf
